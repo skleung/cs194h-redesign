9683feda79083ad9a42ffe579557e85e7b622f89
Sherman Leung <shermankleung@gmail.com>
2016-09-16 18:02:12 -0400

    updated script to populate projects

--- data/data.js
+++ data/data.js
@@ -1,17 +1,22 @@
 // The sole purpose of this file is to hold the data that changes from year to year. Modify the JS objects below as necessary so that Handlebars can render them properly.
 
-var projects = [
-    {
-      "link":"#",
-      "title": "Sample Project",
-      "tagline": "#wethebest",
-      "students": ["Student 1", "Student 2", "Student 3", "Student 4"]
-    }, {
-      "link":"#",
-      "title": "Sample Project",
-      "tagline": "#another1",
-      "students": ["Student 1", "Student 2", "Student 3", "Student 4"]
-    }
+var studios = [
+  {
+    name: "Health",
+    projects: [
+      {
+        "link":"#",
+        "title": "Sample Project",
+        "tagline": "#wethebest",
+        "students": ["Student 1", "Student 2", "Student 3", "Student 4"]
+      }, {
+        "link":"#",
+        "title": "Sample Project",
+        "tagline": "#another1",
+        "students": ["Student 1", "Student 2", "Student 3", "Student 4"]
+      }
+    ]
+  }
 ];
 
 var students = [
