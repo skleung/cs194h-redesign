a865668d7bcae552df16912596bbd2c5275cbbd1
Sherman Leung <shermankleung@gmail.com>
2016-09-27 13:25:23 -0700

    wills changes

--- data/data.js
+++ data/data.js
@@ -2,20 +2,61 @@
 
 var studios = [
   {
-    name: "Health",
+    name: "Health (Sherman)",
+    blurb: "Digital and mobile health are some of the fastest growing industries of our day. As the gap between healthcare and technology is closing, patients and physicians alike are turning to technology-based products to enable more accessible and effective healthcare. From mobile apps that help patients manage their medications to tools that help resource-constrained clinics manage patient pipelines, this intersection of health and technology is ripe with opportunity!",
     projects: [
-      {
-        "link":"#",
-        "title": "Sample Project",
-        "tagline": "#wethebest",
-        "students": ["Student 1", "Student 2", "Student 3", "Student 4"]
-      }, {
-        "link":"#",
-        "title": "Sample Project",
-        "tagline": "#another1",
-        "students": ["Student 1", "Student 2", "Student 3", "Student 4"]
-      }
+      // {
+      //   "link":"#",
+      //   "title": "Sample Project",
+      //   "tagline": "#wethebest",
+      //   "students": ["Student 1", "Student 2", "Student 3", "Student 4"]
+      // }, {
+      //   "link":"#",
+      //   "title": "Sample Project",
+      //   "tagline": "#another1",
+      //   "students": ["Student 1", "Student 2", "Student 3", "Student 4"]
+      // }
     ]
+  },
+  {
+    name: "Learning/Education (Emily)",
+    blurb: "We are constantly learning by acquiring new skills, knowledge, and behaviors. In this studio, we will think about how technology can enable, supplement, or support learning. How can we use technology to support under-resourced demographics or accommodate different learning styles and needs?  What kinds of challenges can we solve to help teachers focus on teaching? Feel free to look at and beyond your own learning environment for inspiration.",
+    projects: []
+  },
+  {
+    name: "Art and Culture (Ash)",
+    blurb: "Art and culture are integral to a vibrant, diverse, and cooperative community. Appreciation and engagement with art and culture spurs personal growth through self-expression and response, and brings people together over common values and shared experiences.In this studio, we will build technology that enables people as creators, communicators, and empathetic citizens through engagement with arts and culture. We will seek opportunities where technology can complement our engagement with topics like literature, design, performance, music, journalism, visual arts, and with public spaces like community centers and organizations, libraries, museums, and historical sites.",
+    projects: []
+  },
+  {
+    name: "Inclusive Design (Kevin)",
+    blurb: 'Autocomplete and voice control began as features for disabled users. Vint Cerf helped invent email in part to communicate with his deaf wife while at work. OCR, now used by Google Books and Translate to convert print and handwritten text to digital form, was invented to create a reading machine for the blind. Look anywhere around you and you’ll find technology originally developed to help underserved populations that now benefits everyone.<br><br>This studio focuses on gaining insights from communities and individuals that might be overlooked by the general population. This includes those that are under-represented when developing technology such as women and people of color. We already have contacts and mentors established in a variety of communities. From this starting point, studio members can explore any number of areas that interest them, from music, wearables, and autonomous cars to anything else you can think of! What can we learn when we design specifically for a single person, perhaps a dyslexic first-grader or a wheelchair-bound relative, instead of for the "average" person? Hopefully, we can improve not only their lives but the lives of everyone around them.',
+    projects: []
+  },
+  {
+    name: "[Micro]Adventure (Kat)",
+    blurb: "Life's about the journey, not the destination, yet too often the potential for adventure inherent in this journey is lost. The hunger to discover is easily forgotten in the clockwork tedium of commutes and errands that powers the hamster wheel of the every day. Even when actively pursued, the delight of exploring is often overwhelmed by a sea of stressful details regarding metro transfers and language barriers. How can we create opportunities for discovery and lower the barrier to exploration? The section seeks design solutions that rekindle playfulness, inspire wonder, and awaken wanderlust, both at home and abroad, to invite users to live more adventurously. &quot;Get out there, take off your shoes, and Bilbo it up!&quot;",
+    projects: []
+  },
+  {
+    name: "Digital Democracy (Shubha) ",
+    blurb: 'Dialogue is the oxygen of democracy, and today, much of that dialogue dwells online. How might we leverage technological tools to broaden the reach of that dialogue to marginalized groups? To enrich the online conversation with fact-checking? To present complex information to our end-users -- voters -- in clear, meaningful ways? As the presidential election draws nearer, these questions are pressing and high-impact. Apart from the election cycle, we might also consider technology-enabled strategies to help citizens engage with local governments, or trawl the voting records of elected officials, for example. Projects in this space could include (with varying scope): <a href=&quot;http://hci.stanford.edu/courses/cs147/2014/au/projects/behavior/papertrail/&quot;>Paper Trail (former CS 147 project)</a>, DoNotPay, FacebookLive, Periscope, etc.',
+    projects: []
+  },
+  {
+    name: "Merging the Real and Digital World: Mixed Reality (John YS)",
+    blurb: 'How can we leverage mixed reality - the merging of real and digital worlds to create solutions to current needs?<br><br>This last summer we saw the first mainstream mixed reality &quot;killer app&quot; when Pokemon Go took the world by storm with hundreds of millions of users and obsessiveness to rival Facebook.  But mixed reality covers a whole range than spans far more than just games: Have you used the virtual avatars and symbols in Waze to navigate around traffic jams and accidents?  The last time you went for a run, did you keep track of your exercise and accomplishments with Runkeeper or spice things up with Zombie Run? Maybe you recently explored a city with Detour? When you moved into your dorm, did you virtually try out your IKEA furniture before heading to the store in East Palo Alto? Did you need a sign translated on your trip overseas this summer? Even finding a restaurant can now get fancy. If you recently saw a house constructed, it could have used HoloBuilder. Have you used a Snapchat filter lately?<br><br>We will work at the bleeding edge of mixed reality in human computer interaction design.  This will be an opportunity to use some of the latest software tools and hardware. It will also require focus and discipline to design and prototype solutions to real world problems and needs at a scope that can be accomplished within the quarter.',
+    projects: []
+  }, 
+  {
+    name: "Food (Will)",
+    blurb: "Everyone needs it, most people love it, but society's general approach to food has remained constant for centuries--three meals a day, eye-balling portion sizes, deciding between cooking at home or eating out, etc. Now, there's a burgeoning ecosystem around &quot;revolutionizing food&quot;--starting at the source (Hampton Creek creating eggless mayo, Beyond Meat culturing meat-tasting matter from plants, etc.), preparation (Blue Apron), delivery (DoorDash, GoodEggs, etc.), consumption (Soylent), and leftovers (Too Good To Go). In this studio, students will focus on learning from the various players in this space, from the producers to the consumers and everyone in between to make valuable contributions to the space.",
+    projects: []
+  },
+  {
+    name: "Home (Edwin)",
+    blurb: 'Internet of Things (IoT) is starting to integrate into our everyday lives, allowing us to control many physical devices, such as thermostats, refrigerators, and even sprinklers from our wrists or phones. Nest has created a thermostat that learns to control temperature and helps you save energy in heating and cooling. Ring has launched a video doorbell that lets you interact with visitors from anywhere with your phone. How can you use technology to make homes more accessible? What are some daily/weekly tasks at home that could be automated and more efficient?',
+    projects: []
   }
 ];
 
